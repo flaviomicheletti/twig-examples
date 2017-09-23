da566d700b85b07529b299906f378e128acf921f
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-09-22 22:35:30 -0300

    add String Interpolation

---
+++ 07-string-interpolation/index.php
@@ -0,0 +1,9 @@
+<?php
+
+require "../vendor/autoload.php";
+
+$loader = new Twig_Loader_Filesystem('./templates');
+$twig = new Twig_Environment($loader);
+
+
+echo $twig->render('index.html', ["bar" => " - "]);
