f327065fdb3e17b38dffc39051bb235da1aac4e7
Flávio Micheletti <flaviomicheletti@hotmail.com>
2017-10-06 12:01:04 -0300

    detalhes

--- 02-simple-example/index.php
+++ 02-simple-example/index.php
@@ -11,6 +11,14 @@ $twig   = new Twig_Environment($loader);
 $flag = (rand(0, 1)) ? true : false;
 $arr  = [rand(1, 10), rand(1, 10), rand(1, 10), rand(1, 10), rand(1, 10)];
 
+
+class Foo {
+    $bar  = 'prop 1';
+    $die  = 'prop 2';
+    $lish = 'prop 3';
+}
+
+
 echo $twig->render('index.html', [
     "flag" => $flag,
     "arr"  => $arr
